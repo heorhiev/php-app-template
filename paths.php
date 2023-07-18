82d06c03273c79f55bda4178b593d9bf8a543f77
dev8 <dev8.sofona@gmail.com>
2023-07-18 23:29:12 +0300

    + vendor alias

--- paths.php
+++ paths.php
@@ -2,9 +2,12 @@
 
 const APP_PATH = __DIR__;
 const CONFIG_PATH = APP_PATH . '/config';
+const VENDOR_PATH = APP_PATH . '/vendor';
+
 const LOGS_PATH = APP_PATH . '/runtime/logs';
 const COMMON_PATH = APP_PATH . '/common';
 
 const ALIASES = [
     '{@configPath}' => CONFIG_PATH,
+    '{@vendorPath}' => VENDOR_PATH,
 ];
